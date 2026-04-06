33c7ac27f4d4d770cbec15a2f232a64d6ff11049
Kaio Nascimento <kaio.seth@gmail.com>
2026-04-06 18:44:09 -0400

    update spaces

--- cpp/funcoes_matematicas.cpp
+++ cpp/funcoes_matematicas.cpp
@@ -6,27 +6,30 @@ int main() {
     int opcao;
 
     do {
-        cout << "=== MENU ===" << endl;
+        cout << "=================" << endl;
+        cout << "====== MENU =====" << endl;
         cout << "1 - Potencia" << endl;
         cout << "2 - Raiz" << endl;
         cout << "3 - Trigonometria" << endl;
         cout << "4 - Logaritmo" << endl;
         cout << "0 - Sair" << endl;
-        cout << "Opcao: ";
+        cout << " " << endl;
+        cout << "Digite a opcao: ";
         cin >> opcao;
 
         if (opcao == 1) {
             double base, expoente;
+            cout << " " << endl;
             cout << "Digite a base: ";
             cin >> base;
             cout << "Digite o expoente: ";
             cin >> expoente;
 
             cout << "Resultado: " << pow(base, expoente) << endl << endl;
-        }
-
-        else if (opcao == 2) {
+            cout << " " << endl;
+        }else if (opcao == 2) {
             double numero;
+            cout << " " << endl;
             cout << "Digite um numero: ";
             cin >> numero;
 
@@ -35,20 +38,20 @@ int main() {
             } else {
                 cout << "Raiz: " << sqrt(numero) << endl << endl;
             }
-        }
-
-        else if (opcao == 3) {
+            cout << " " << endl;
+        }else if (opcao == 3) {
             double angulo;
+            cout << " " << endl;
             cout << "Digite o angulo (radianos): ";
             cin >> angulo;
 
             cout << "Seno: " << sin(angulo) << endl;
             cout << "Cosseno: " << cos(angulo) << endl;
             cout << "Tangente: " << tan(angulo) << endl << endl;
-        }
-
-        else if (opcao == 4) {
+            cout << " " << endl;
+        }else if (opcao == 4) {
             double numero;
+            cout << " " << endl;
             cout << "Digite um numero positivo: ";
             cin >> numero;
 
@@ -57,15 +60,17 @@ int main() {
             } else {
                 cout << "Log: " << log(numero) << endl << endl;
             }
-        }
-
-        else if (opcao != 0) {
+            cout << " " << endl;
+        }else if (opcao != 0) {
+            cout << " " << endl;
             cout << "Opcao invalida!" << endl << endl;
+            cout << " " << endl;
         }
 
     } while (opcao != 0);
 
-    cout << "Encerrando..." << endl;
+    cout << " " << endl;
+    cout << "Encerrando a execução..." << endl;
 
     return 0;
 }
